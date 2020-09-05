665faa7b0d4c76b8247b631e22c246fcbd68c695
William Moore <caranmegil@gmail.com>
2020-09-05 18:11:29 -0500

    Switch from Firebase for permission configuration to Consul

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var admin = require("firebase-admin");
 
 var consul = require('consul')(
   {
@@ -11,54 +10,19 @@ var consul = require('consul')(
 var serviceAccount = {};
 var db = undefined;
 
-consul.kv.get('aethred/db/serviceAccountKey', (err, res) => {
-  if(err) throw err;
-  if (!res.Value) console.error("unexpected error")
-  serviceAccount = JSON.parse(res.Value);
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://aethreddb.firebaseio.com"
-  });
-  db = admin.database();
-})
-
-
 /* GET home page. */
 router.get('/:service/:user', function(req, res, next) {
   try {
-  var ref = db.ref(`permissions/${req.params.service}/${req.params.user}`)
-  ref.once('value', (snapshot) => {
-    var userPermissions = snapshot.val()
-    var permissions = Object.keys(userPermissions).filter( (k) => {return userPermissions[k]} )
-    res.json({results: permissions})
-  })
+    consul.kv.get('aethred/permissions', (err, resp) => {
+      if(err) throw err;
+      if (!resp.Value) console.error("unexpected error")
+      let users = JSON.parse(resp.Value)[req.params.service] || {}
+      let permissions = users[req.params.user] || [] 
+      res.json({results: permissions})
+    })
   } catch {
     res.status(500)
   }
-});
-
-router.post('/:service/:user', function(req, res, next) {
-  var ref = db.ref(`permissions/${req.params.service}/${req.params.user}`)
-
-  var userPermissions = {}
-  req.body.permissions.forEach( (k) => {
-    userPermissions[k] = true
-  })
-
-  ref.update(userPermissions)
-  res.json({permissions: req.body.permissions})
-});
-
-router.delete('/:service/:user', function(req, res, next) {
-  var ref = db.ref(`permissions/${req.params.service}/${req.params.user}`)
-
-  var userPermissions = {}
-  req.body.permissions.forEach( (k) => {
-    userPermissions[k] = false
-  })
-
-  ref.update(userPermissions)
-  res.json({permissions: req.body.permissions})
 });
 
 module.exports = router;
