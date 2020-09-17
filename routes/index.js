bc452c86ac26de1c9c940e09190de5cf419192a7
William Moore <caranmegil@gmail.com>
2020-09-16 20:32:22 -0500

    Fix an extra z

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ var fs = require('fs');
 /* GET home page. */
 router.get('/:service/:user', function(req, res, next) {
   try {
-    z
     let rawdata = fs.readFileSync(process.env.PERM_FILE)
     let users = JSON.parse(rawdata)[req.params.service] || {}
     let permissions = users[req.params.user] || [] 
