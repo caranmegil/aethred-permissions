6e66059acb5294939b02d7cc613578cae0f64e59
William Moore <caranmegil@gmail.com>
2020-09-17 15:49:47 -0500

    Put the permission referesh on an interval

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,16 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
+var users = {}
+
+setInterval( () => {
+  let rawdata = fs.readFileSync(process.env.PERM_FILE)
+  users = JSON.parse(rawdata)[req.params.service] || {}
+},10000)
+
 /* GET home page. */
 router.get('/:service/:user', function(req, res, next) {
   try {
-    let rawdata = fs.readFileSync(process.env.PERM_FILE)
-    let users = JSON.parse(rawdata)[req.params.service] || {}
     let permissions = users[req.params.user] || [] 
     res.json({results: permissions})
   } catch {
