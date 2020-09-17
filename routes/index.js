935564ecee32e2d93eedda54f0c6a4594fd68e8d
William Moore <caranmegil@gmail.com>
2020-09-16 20:08:55 -0500

    use a permissions file instead of consul

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
-var consul = require('consul')(
-  {
-    host: process.env.CONSUL_HOST
-  }
-)
-
-var serviceAccount = {};
-var db = undefined;
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/:service/:user', function(req, res, next) {
   try {
-    consul.kv.get('aethred/permissions', (err, resp) => {
-      if(err) throw err;
-      if (!resp.Value) console.error("unexpected error")
-      let users = JSON.parse(resp.Value)[req.params.service] || {}
-      let permissions = users[req.params.user] || [] 
-      res.json({results: permissions})
-    })
+    let rawdata = fs.readFileSync(process.env.PERM_FILE)
+    let users = JSON.parse(rawdata) || {}
+    let permissions = users[req.params.user] || [] 
+    res.json({results: permissions})
   } catch {
     res.status(500)
   }
